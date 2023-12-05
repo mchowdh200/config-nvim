5efd56841ed51f9ddde0068333ac84be334f6edb
Murad Chowhdury <murad@biot22-144-dhcp.int.colorado.edu>
2023-12-05 16:49:35 -0700

    forgot to add a config file

---
+++ lua/config/tokyonight.lua
@@ -0,0 +1,26 @@
+-- change some of the colors to be more readable
+require("tokyonight").setup({
+    styles = {
+        functions = { bold = true },
+        keywords = { bold = true },
+        -- variables = { italic = true },
+        conditionals = { bold = true, italic=false },
+        loops = { bold = true, italic=false },
+        comments = { italic = true },
+    }
+})
+
+
+-- example
+-- comments = { "italic" },
+-- conditionals = { "italic" },
+-- loops = {},
+-- functions = {},
+-- keywords = {},
+-- strings = {},
+-- variables = {},
+-- numbers = {},
+-- booleans = {},
+-- properties = {},
+-- types = {},
+-- operators = {},
